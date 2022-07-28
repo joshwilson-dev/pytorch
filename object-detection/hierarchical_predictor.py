00f80ec8029b6d5c3dfce1dc39b57e1d7d55aaa0
Joshua Wilson <joshua.wilson1@uq.net.au>
2022-07-28 16:59:15 +1000

    attempting to get hierarchical model working

---
+++ object-detection/hierarchical_predictor.py
@@ -0,0 +1,36 @@
+from torch import nn
+import torch
+from torchvision.models._meta import _COCO_CATEGORIES
+
+class HFastRCNNPredictor(nn.Module):
+    """
+    Standard classification + bounding box regression layers
+    for Fast R-CNN.
+
+    Args:
+        in_channels (int): number of input channels
+        num_classes (int): number of output classes (including background)
+    """
+
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        # self.cls_score = nn.Linear(in_channels, num_classes) #hierarchical goes here
+        self.bbox_pred = nn.Linear(in_channels, num_classes * 4)
+
+    def forward(self, x):
+        if x.dim() == 4:
+            torch._assert(
+                list(x.shape[2:]) == [1, 1],
+                f"x has the wrong shape, expecting the last two dimensions to be [1,1] instead of {list(x.shape[2:])}",
+            )
+        x = x.flatten(start_dim=1)
+        scores = self.cls_score(x)
+        bbox_deltas = self.bbox_pred(x)
+
+        return scores, bbox_deltas
+
+
+_COMMON_META = {
+    "categories": _COCO_CATEGORIES,
+    "min_size": (1, 1),
+}
