a1569ef45e9bf7fbd8324ae260856e518daf2dd8
Joshua Wilson <joshua.wilson1@uq.net.au>
2022-12-23 13:36:56 +1000

    redefining pytorch packages locally rather than editing package

--- code/main.py
+++ code/main.py
@@ -14,82 +14,18 @@ import csv
 import math
 import pandas as pd
 from torchvision.models.detection import roi_heads
-import torch.nn.functional as F
+import custom_roi_heads
+import custom_boxes
 from torchvision.ops import boxes as box_ops
-import itertools
 
-# redefining the postprocess_detctions function to include
-# filtering
-def postprocess_detections(
-    self,
-    class_logits,  # type: Tensor
-    box_regression,  # type: Tensor
-    proposals,  # type: List[Tensor]
-    image_shapes,  # type: List[Tuple[int, int]]
-):
-    # type: (...) -> Tuple[List[Tensor], List[Tensor], List[Tensor]]
-    device = class_logits.device
-    num_classes = class_logits.shape[-1]
+# redefining roi_heads to return scores for all classes
+# and filter the classes based on a supplied filter
+roi_heads.RoIHeads.postprocess_detections = custom_roi_heads.postprocess_detections
+roi_heads.RoIHeads.forward = custom_roi_heads.forward
 
-    boxes_per_image = [boxes_in_image.shape[0] for boxes_in_image in proposals]
-    pred_boxes = self.box_coder.decode(box_regression, proposals)
-
-    # filter the logits, apply softmax, restore filtered scores as 0
-    filter_index = self.filter.nonzero().squeeze()
-    class_logits_filtered = class_logits[:, filter_index]
-    src = F.softmax(class_logits_filtered, -1)
-    index = filter_index.repeat(len(class_logits), 1)
-    pred_scores = torch.zeros(len(class_logits), len(self.filter)).to(torch.device("cuda")).scatter_(1, index, src)
-
-    pred_boxes_list = pred_boxes.split(boxes_per_image, 0)
-    pred_scores_list = pred_scores.split(boxes_per_image, 0)
-
-    all_boxes = []
-    all_scores = []
-    all_labels = []
-    all_class_scores = []
-    for boxes, scores, image_shape in zip(pred_boxes_list, pred_scores_list, image_shapes):
-        boxes = box_ops.clip_boxes_to_image(boxes, image_shape)
-
-        # create labels for each prediction
-        labels = torch.arange(num_classes, device=device)
-        labels = labels.view(1, -1).expand_as(scores)
-
-        # remove predictions with the background label
-        boxes = boxes[:, 1:]
-        scores = scores[:, 1:]
-        labels = labels[:, 1:]
-
-        # batch everything, by making every class prediction be a separate instance
-        boxes = boxes.reshape(-1, 4)
-        class_scores = scores.repeat_interleave(5, dim = 0) # JW keeping all scores
-        scores = scores.reshape(-1)
-        labels = labels.reshape(-1)
-        # print("\n", scores)
-        # print(class_scores)
-
-        # remove low scoring boxes
-        # inds = torch.where(torch.max(scores, 1).values > self.score_thresh)[0] # attempt to keep all scores
-        inds = torch.where(scores > self.score_thresh)[0]
-        boxes, scores, labels, class_scores = boxes[inds], scores[inds], labels[inds], class_scores[inds]
-
-        # remove empty boxes
-        keep = box_ops.remove_small_boxes(boxes, min_size=1e-2)
-        boxes, scores, labels, class_scores = boxes[keep], scores[keep], labels[keep], class_scores[keep]
-
-        # non-maximum suppression, independently done per class
-        keep = box_ops.batched_nms(boxes, scores, labels, self.nms_thresh)
-        # keep only topk scoring predictions
-        keep = keep[: self.detections_per_img]
-        boxes, scores, labels, class_scores = boxes[keep], scores[keep], labels[keep], class_scores[keep]
-
-        all_boxes.append(boxes)
-        all_scores.append(scores)
-        all_labels.append(labels)
-        all_class_scores.append(class_scores)
-
-    return all_boxes, all_scores, all_labels, all_class_scores
-roi_heads.RoIHeads.postprocess_detections = postprocess_detections
+# redefining boxes to do nms on all classes, not class specific
+box_ops.batched_nms = custom_boxes.batched_nms
+box_ops._batched_nms_coordinate_trick = custom_boxes._batched_nms_coordinate_trick
 
 def get_xmp(image_file_path):
     # get xmp information
@@ -270,7 +206,7 @@ def detect_birds(kwargs, image_path, model, device, index_to_class, overlap, pat
             scores = torch.cat((scores, batch_scores.to(torch.device("cpu"))), 0)
             class_scores = torch.cat((class_scores, batch_class_scores.to(torch.device("cpu"))), 0)
             labels = torch.cat((labels, batch_labels.to(torch.device("cpu"))), 0)
-    nms_indices = torchvision.ops.batched_nms(boxes, scores, labels, kwargs["box_nms_thresh"])
+    nms_indices = box_ops.batched_nms(boxes, scores, labels, kwargs["box_nms_thresh"])
     boxes = boxes[nms_indices]
     scores = scores[nms_indices].tolist()
     class_scores = class_scores[nms_indices].tolist()
