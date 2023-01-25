0f29cd00fb851b216aa1ed55865f6701158c00c5
Joshua Wilson <joshua.wilson1@uq.net.au>
2023-01-25 11:46:55 +1000

    simple artificial working

--- code/trial.py
+++ code/trial.py
@@ -8,14 +8,14 @@ width, height = shadow.size
 shadow_data = []
 for item in shadow.getdata():
     if item[3] == 255:
-        shadow_data.append((0, 0, 0, int(255/2)))
+        shadow_data.append((0, 0, 0, int(200)))
     else:
         shadow_data.append(item)
 shadow.putdata(shadow_data)
-x_offset = random.randint(-10, 10)
-y_offset = random.randint(-10, 10)
-
-shadow = shadow.crop((min(x_offset, 0), min(y_offset, 0), max(width, width + x_offset), max(height, height + x_offset)))
+max_offset = 1.25
+x_offset = random.randint(int(-width * max_offset), int(width * max_offset))
+y_offset = random.randint(int(-height * max_offset), int(height * max_offset))
+shadow = shadow.crop((min(x_offset, 0), min(y_offset, 0), max(width, width + x_offset), max(height, height + y_offset)))
 
 shadow.paste(instance, (max(x_offset, 0), max(y_offset, 0)), instance)
 instance = shadow.copy()
