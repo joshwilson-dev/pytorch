1ffd161a9c52d7827fcf203b9ca248981f32275a
Joshua Wilson <uqjwil54@uq.edu.au>
2024-05-06 16:37:54 +1000

    evalimganalysis

---
+++ code/analysis/SC-evalimg_process.R
@@ -0,0 +1,65 @@
+# Title:        
+# Description:  
+# Author:       Joshua P. Wilson
+# Date:         14/02/2024
+
+# Load environment
+renv::load("C:/Users/uqjwil54/Documents/Projects/venvs/DBBD")
+
+# Install required packages
+# install.packages("tidyverse")
+# install.packages("readxl")
+
+# Load required libraries
+library(tidyverse)
+library(readxl)
+
+# Clear the R environment
+rm(list = ls())
+
+# Import data from excel
+root <- "C:/Users/uqjwil54/Documents/Projects/DBBD/balanced-2024_05_01/model-2024_05_06/data.xlsx"
+dataset <- read_excel(path = root, sheet = "ST1-dataset")
+cocoevalimg <- read_excel(path = root, sheet = "ST3-cocoevalimg")
+catId_to_class <- read_excel(path = root, sheet = "ST5-catId_to_class") %>%
+    mutate(class = paste(name, "-", age)) %>%
+    select(class, catId) %>%
+    rbind(data.frame(class = c("classification", "detection"), catId = c(-2, -1)))
+
+# Calculate performance metrics at threshold
+data <- cocoevalimg %>%
+    filter(
+        dataset == 'test',
+        area == "[0, 10000000000]") %>%
+    mutate(error = case_when(
+        det_type == 'gt' ~ '',
+        Matches > 0 ~ 'TP',
+        T ~ 'FP')) %>%
+        # det_type == 'gt' ~ 'FN',
+        # det_type != 'gt' ~ 'FP')) %>%
+    group_by(catId) %>%
+    arrange(catId, desc(Scores), det_type) %>%
+    mutate(gt = sum(det_type == "gt")) %>%
+    # filter(det_type == 'dt') %>%
+    mutate(
+        tp = case_when(error == 'TP' ~ 1, T ~ 0),
+        fp = case_when(error == 'FP' ~ 1, T ~ 0),
+        tp = cumsum(tp),
+        fp = cumsum(fp),
+        r = floor(tp / gt * 100)/100,
+        p = tp / (tp + fp)) %>%
+    arrange(catId, desc(r)) %>%
+    mutate(p = cummax(p)) %>%
+    arrange(catId, r) %>%
+    group_by(catId, r) %>%
+    slice(1) %>%
+    group_by(catId) %>%
+    select(catId, r, p) %>%
+    complete(r = round(seq(0, 1, 0.01), 2)) %>%
+    arrange(catId, desc(r)) %>%
+    fill(p) %>%
+    mutate(p = case_when(is.na(p) ~ 0, T ~ p))
+
+AP <- data %>%
+    group_by(catId) %>%
+    summarise(AP = mean(p))
